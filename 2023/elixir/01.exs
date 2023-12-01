6134eeccd4d5030bc9f7309b7f6a080c46ca9319
Jarang <jaran.godejord@gmail.com>
2023-12-01 13:56:39 +0100

    elixir day 01

--- 2023/elixir/01.exs
+++ 2023/elixir/01.exs
@@ -2,14 +2,6 @@ defmodule D do
   @input File.read!("../input_01.txt")
   @regex_decimals ~r/\d/
   @regex_all_numbers ~r/(?=(one|two|three|four|five|six|seven|eight|nine|\d))/
-  @sample_input "two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen"
-
 
   def t1 do
     @input
@@ -27,8 +19,6 @@ zoneight234
     end)
   end
 
-
-  #def str_to_num("zero"),  do: "0"
   def str_to_num("one"),   do: "1"
   def str_to_num("two"),   do: "2"
   def str_to_num("three"), do: "3"
@@ -49,18 +39,12 @@ zoneight234
           |> Enum.filter(& &1 != "")
       [a] = Enum.take(nums, 1)
       [b] = Enum.take(nums, -1)
-      IO.inspect nums
-      IO.inspect(a)
-      IO.inspect(b)
       a = a |> str_to_num()
       b = b |> str_to_num()
 
       val = a <> b 
         |> String.to_integer()
-
-      IO.inspect val
-      IO.puts "___________"
-
+      
       val + acc
     end)
   end
