55efc48448746ca7b5e2ad1bb8b769663bb810bb
Jarang <jaran.godejord@gmail.com>
2023-12-01 23:39:52 +0100

    elixir day 007 t1 improvements

--- 2015/elixir/07.exs
+++ 2015/elixir/07.exs
@@ -1,14 +1,5 @@
 defmodule D do
   @input File.read!("../input_07.txt")
-  @sample_input "123 -> x
-456 -> y
-x AND y -> d
-x OR y -> e
-x LSHIFT 2 -> f
-y RSHIFT 2 -> g
-NOT x -> h
-NOT y -> i"
-
   @fun_map %{
     "AND"    => &Bitwise.band/2,
     "NOT"    => &Bitwise.bnot/1,
@@ -17,49 +8,8 @@ NOT y -> i"
     "RSHIFT" => &Bitwise.bsr/2,
   }
 
-  
-  def parse([value, "->", dest], acc) do 
-    Map.put(acc, dest, String.to_integer(value))
-  end
-
-  def parse([value, "AND", wire2, "->", dest], acc) when is_number(value) do 
-    value = String.to_integer(value)
-    wire2 = Map.get(acc, wire2)
-    Map.put(acc, dest, Bitwise.band(value, wire2))
-  end
-
-  def parse([wire1, "AND", wire2, "->", dest], acc) do 
-    wire1 = Map.get(acc, wire1)
-    wire2 = Map.get(acc, wire2)
-    Map.put(acc, dest, Bitwise.band(wire1, wire2))
-  end
-  
-  def parse([wire1, "OR", wire2, "->", dest], acc) do 
-    wire1 = Map.get(acc, wire1)
-    wire2 = Map.get(acc, wire2)
-    Map.put(acc, dest, Bitwise.bor(wire1, wire2))
-  end
-
-  def parse([wire1, "LSHIFT", value, "->", dest], acc) do 
-    wire1 = Map.get(acc, wire1)
-    value = String.to_integer(value)
-    Map.put(acc, dest, Bitwise.bsl(wire1, value))
-  end
-
-  def parse([wire1, "RSHIFT", value, "->", dest], acc) do 
-    wire1 = Map.get(acc, wire1)
-    value = String.to_integer(value)
-    Map.put(acc, dest, Bitwise.bsr(wire1, value))
-  end
-
-  def parse(["NOT", wire1, "->", dest], acc) do 
-    wire1 = Map.get(acc, wire1)
-    Map.put(acc, dest, Bitwise.bnot(wire1))
-  end
-
-
   def solve(instructions), do: solve(instructions, %{}, [])
-  def solve([], table, []), do: table # |> Map.get("a")
+  def solve([], table, []), do: table
   def solve([], table, instructions), do: solve(instructions, table, [])
   
   # Handles ["1600", "->", "u"]
@@ -78,12 +28,12 @@ NOT y -> i"
   end
 
   # Handles ["NOT", "lo", "->", "ed"] 
-  def solve([i = [_, wire, _, dest] | rest], table, instructions) do
+  def solve([i = [op, wire, _, dest] | rest], table, instructions) do
     case Map.has_key?(table, wire) do
       true -> 
         n = Map.get(table, wire)
-        n = Bitwise.bnot(n)
-        solve(rest, Map.put(table, dest, n), instructions)
+        result = @fun_map[op].(n)
+        solve(rest, Map.put(table, dest, result), instructions)
       false -> solve(rest, table, [i | instructions])
     end
   end
@@ -94,53 +44,32 @@ NOT y -> i"
   # Handles ["km", "AND", "ij", "->", "hv"]
   # Handles ["1", "AND", "il", "->", "ag"]
   def solve([i = [n, op, v, _, dest] | rest], table, instructions) do
-    case Integer.parse(n) do
-      # Handles 5
-      {n, _} -> 
-        case Map.has_key?(table, v) do
-          true -> 
-            v = Map.get(table, v)
-            n = Bitwise.band(n, v)
-            solve(rest, Map.put(table, dest, n), instructions)
-          false -> solve(rest, table, [i | instructions])
-        end
-      :error -> 
-        case Integer.parse(v) do
-          {v, _ } -> 
-            case Map.has_key?(table, n) do
-              true ->
-                # Handles 1 and 2
-                n = Map.get(table, n)
-                val = @fun_map[op].(n, v)
-                solve(rest, Map.put(table, dest, val), instructions)
-              false -> solve(rest, table, [i | instructions])
-            end
-          :error ->
-            # Handles 3 and 4
-            case has_keys?(table, [n, v]) do
-              true -> 
-                [n, v] = get_values(table, [n, v])
-                val = @fun_map[op].(n, v)
-                solve(rest, Map.put(table, dest, val), instructions)
-              false -> solve(rest, table, [i | instructions])
-            end
-        end
-    end
+    [n, v] = to_int_or_key(n, v)
+    case has_keys?(table, [n, v]) do
+      true -> 
+        [n, v] = get_values(table, [n, v])
+        result = @fun_map[op].(n, v)
+        solve(rest, Map.put(table, dest, result), instructions)
+      false -> solve(rest, table, [i | instructions])
+      end
   end
 
   def has_keys?(m, keys) do
-    Enum.map(keys, fn key -> 
-      Map.has_key?(m, key)
-    end)
-    |> Enum.all?()
+    Enum.all?(keys, fn key -> (Map.has_key?(m, key) || is_integer(key))  end)
   end
 
   def get_values(m, keys) do
-    Enum.map(keys, fn key ->
-      Map.get(m, key)
-    end)
+    Enum.map(keys, fn key -> if is_binary(key), do: Map.get(m, key), else: key end)
   end
 
+  def to_int_or_key(n, v) do
+    case {Integer.parse(n), Integer.parse(v)} do
+      {{i, _}, {j, _}} -> [i, j]
+      {{i, _}, :error} -> [i, v]
+      {:error, {j, _}} -> [n, j]
+      {:error, :error} -> [n, v]
+    end
+  end
 
   def parse_input() do
     @input
@@ -152,7 +81,7 @@ NOT y -> i"
   # Solved when no more instructions
   # input = instructions
   # if can do instruction: do it, and next
-  # else put back into insctruction and try next
+  # else put back into instruction and try next
 
   def t1 do
     parse_input()
@@ -165,6 +94,5 @@ NOT y -> i"
     parse_input()
     |> solve()
     |> Map.get("a")
-    
   end  
 end
