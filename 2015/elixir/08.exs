40630bf58bdcf5b6194a9d65816d11092340d37d
Jarang <jaran.godejord@gmail.com>
2024-10-15 18:05:00 +0200

    elixir day 08

--- 2015/elixir/08.exs
+++ 2015/elixir/08.exs
@@ -4,8 +4,9 @@ defmodule D do
   def parse() do
     @input
     |> String.split("\n", trim: true)
-    |> Enum.map(&String.split(&1, "", trim: true))
-    |> IO.inspect()
+
+    # |> Enum.map(&String.split(&1, "", trim: true))
+    # |> IO.inspect()
   end
 
   def parse2() do
@@ -13,24 +14,80 @@ defmodule D do
     |> String.split("\n", trim: true)
   end
 
-  
-  def run(list), do: run(list, 0)
-  def run([], acc), do: acc
-  def run(["\\", "x", a, b  | rest], acc), do: run(rest, acc + 6  - 1)
-  def run(["\\", "\"" | rest], acc), do: run(rest, acc + 2 - 1)
-  def run(["\\" | rest], acc), do: run(rest, acc + 2 - 1)
-  def run(["\"" | rest], acc), do: run(rest, acc + 1 - 0)
-  def run([_ | rest], acc), do: run(rest, acc + 1 - 0)
+  def calc_code(list), do: calc_code(list, 0)
+  def calc_code([], acc), do: acc
+  # \x03
+  def calc_code(["\\", "x", _, _ | rest], acc), do: calc_code(rest, acc + 4)
+  # \"
+  def calc_code(["\\", "\"" | rest], acc), do: calc_code(rest, acc + 2)
+  # \\
+  def calc_code(["\\", "\\" | rest], acc), do: calc_code(rest, acc + 2)
+  # " at the start and end of the line
+  def calc_code(["\"" | rest], acc), do: calc_code(rest, acc + 1)
+  # any other character
+  def calc_code([_ | rest], acc), do: calc_code(rest, acc + 1)
+
+
+  def calc_data(list), do: calc_data(list, 0)
+  def calc_data([], acc), do: acc
+  # \x03
+  def calc_data(["\\", "x", _, _ | rest], acc), do: calc_data(rest, acc + 1)
+  # \"
+  def calc_data(["\\", "\"" | rest], acc), do: calc_data(rest, acc + 1)
+  # \\
+  def calc_data(["\\", "\\" | rest], acc), do: calc_data(rest, acc + 1)
+  # " at the start and end of the line
+  def calc_data(["\"" | rest], acc), do: calc_data(rest, acc + 0)
+  # any other character
+  def calc_data([_ | rest], acc), do: calc_data(rest, acc + 1)
 
   def t1 do
     parse()
-    |> run()
+    |> Enum.map(fn x ->
+      code =
+        x
+        |> String.split("", trim: true)
+        |> calc_code()
+
+      data =
+        x
+        |> String.split("", trim: true)
+        |> calc_data()
+      # IO.inspect("#{x}; code: #{code} - data: #{data}")
+      code - data
+    end)
+    |> Enum.sum()
   end
 
-  def t0 do
-    
+
+  def calc_new_code(list), do: calc_new_code(list, 0)
+  def calc_new_code([], acc), do: acc
+  # \x03
+  def calc_new_code(["\\", "x", _, _ | rest], acc), do: calc_new_code(rest, acc + 5)
+  # \"
+  def calc_new_code(["\\", "\"" | rest], acc), do: calc_new_code(rest, acc + 4)
+  # \\
+  def calc_new_code(["\\", "\\" | rest], acc), do: calc_new_code(rest, acc + 4)
+  # " at the start and end of the line
+  def calc_new_code(["\"" | rest], acc), do: calc_new_code(rest, acc + 3)
+  # any other character
+  def calc_new_code([_ | rest], acc), do: calc_new_code(rest, acc + 1)
+
+  def t2 do
+    parse()
+    |> Enum.map(fn x ->
+      new_code =
+        x
+        |> String.split("", trim: true)
+        |> calc_new_code()
+
+      code =
+        x
+        |> String.split("", trim: true)
+        |> calc_code()
+      # IO.inspect("#{x}; new_code: #{new_code} - code: #{code}")
+      new_code - code
+    end)
+    |> Enum.sum()
   end
-
-
-  
 end
